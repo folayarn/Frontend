dee94ddc9fe07f92ee7c747795a6f932bf5cdd17
Folayan Adesola <48876487+folayarn@users.noreply.github.com>
2020-08-02 00:58:49 +0100

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import {Redirect } from 'react-router-dom'
-import { Row,Col,Form} from 'react-bootstrap'
+import { Row,Col,Form,Button} from 'react-bootstrap'
 import axios from 'axios'
 import Documentation from './Documentation'
 import Skeleton from "react-loading-skeleton";
@@ -15,14 +15,15 @@ this.state={
   id:'',
   name:'',
   email:'',
-  loading:true
+  loading:true,
+  isnotlog:false
          }
-         
+         this.logout=this.logout.bind(this)
      }
  
  
      componentDidMount(){
-         axios.get('https://backendj.herokuapp.com/api/user',{
+         axios.get('http://localhost:8000/api/user',{
              headers:{Authorization: `Bearer ${localStorage.usertoken}`}
  
          }).then((res)=>{
@@ -31,9 +32,10 @@ this.state={
              id:res.data.id,
              email:res.data.email,
              loading:false
+             
              })
          })
-     }
+        }
  
  
    
@@ -42,6 +44,13 @@ this.state={
  
  
  
+        logout=(e)=>{
+          e.preventDefault()
+      localStorage.removeItem('usertoken')
+      this.setState({
+       isnotlog:true
+      })
+      }
  
  
      
@@ -49,16 +58,18 @@ this.state={
 
 render(){
 
-  const {id,name,email,}=this.state
+  const {id,name,email,isnotlog}=this.state
 
-         if(localStorage.usertoken){
+         if(localStorage.usertoken||!isnotlog ){
   return (
     <div>
-    <Col>
 <Row>
-<Col md={2} sm={2} xs={2}>
+<Col md={2} sm={1} xs={1}>
+ <Button className="drop" size='sm' variant="danger" onClick={this.logout.bind(this)}>
+ <span className="fa fa-sign-out"></span>Logout</Button>
+ 
 </Col>
-<Col md={8} sm={8} xs={8} className="apiback">
+<Col md={8} sm={10} xs={10} className="apiback">
 <h1>welcome home!!</h1>
 {this.state.loading?
 
@@ -86,7 +97,7 @@ render(){
   <Form>
     <Form.Group>
 
-<Form.Control value={localStorage.usertoken} as="textarea" cols={60} rows={10}/>
+<Form.Control value={localStorage.usertoken} as="textarea"  cols={60} rows={10}/>
 </Form.Group>
 </Form>
 
@@ -97,12 +108,10 @@ render(){
 }
 
 </Col>
-
-
-<Col md={2} sm={2} xs={2}></Col>
+<Col md={2} sm={1} xs={1}></Col>
 </Row>
-</Col>
-<Col>
+
+<Col md={12} sm={12} xs={12}>
 <Row>
   <Documentation/>
 </Row>
